3d242da4598f06f266fa379b33f6a299791ffb42
MarceloBill84 <marcelobill@msn.com>
2024-07-13 17:25:16 -0300

    Creating hello world project

---
+++ lib/gradient_container.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/styled_text.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key, required this.gradientColors});
+
+  final List<Color> gradientColors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: gradientColors,
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: StyledText(text: 'Hello World!'),
+      ),
+    );
+  }
+}
